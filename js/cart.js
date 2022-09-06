d242a86e93dc6abd53073e1f0f177546cf89355c
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-06 02:00:34 +0200

    Display the product and quantity to the ui done

--- js/cart.js
+++ js/cart.js
@@ -14,10 +14,23 @@ const addToCart = ()=>{
     const product = getInputFieldValueById('product-field');
     const quantity = getInputFieldValueById('quantity-field');
 
+    // ** display product and quantity 
+    displayCartItems(product,quantity)
+
 };
 
 // ** display the product and quantity
 
-const displayCartItems = ()=>{
+const displayCartItems = (product,quantity)=>{
     // ** where to display
-}
+
+    const productsContainer = document.getElementById('display-container');
+
+    const li = document.createElement('li');
+
+    li.innerText = `${product} ${quantity}`;
+
+    productsContainer.appendChild(li)
+};
+
+// ** Local Storage Option
